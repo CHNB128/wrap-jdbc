41f03a53ce77c227214b73e44e782fc2607a56ad
Daren <drake.aren@protonmail.com>
2019-09-29 03:15:57 +0600

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wrap-jdbc "0.1.0"
+(defproject wrap-jdbc "0.1.1"
   :description "Wrap jdbc connection to your compojure handler"
   :url "https://github.com/CHNB128/wrap-jdbc"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
